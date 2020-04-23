29a77225e37955b50797479402c7f6961bca423d
MechanicalCoder <chandan.sinha547@gmail.com>
2020-04-23 16:34:24 +0000

    Deploying to gh-pages from  @ 1592e9177d3122dab5983cfef07cee793c0d20b2 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3078b1c3e3a4cee6055b6aaf6b5e2bca.js"
+  "/precache-manifest.970cb8ddc8d447301684daa0f4d4de4b.js"
 );
 
 self.addEventListener('message', (event) => {
